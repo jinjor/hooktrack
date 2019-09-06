f5e72f4bb1e7996e00d403bf94dad9389c5c2f53
jinjor <jinjorweb@gmail.com>
2019-09-07 03:40:39 +0900

    update ops

---
+++ ops/create-db.ts
@@ -0,0 +1,30 @@
+import * as faunadb from "faunadb";
+
+const secret = process.env.FAUNADB_ADMIN_SECRET || "";
+console.log("FAUNADB_ADMIN_SECRET:", secret.replace(/\w/g, "*"));
+if (!secret) {
+  console.log("FAUNADB_ADMIN_SECRET is not set.");
+  process.exit(1);
+}
+
+const dbName = process.argv[2];
+console.log("dbName:", dbName);
+if (!dbName) {
+  console.log("dbName is not set.");
+  process.exit(1);
+}
+
+const q = faunadb.query as any;
+const client = new faunadb.Client({ secret });
+
+(async () => {
+  await client.query(
+    q.CreateDatabase({
+      name: dbName,
+      priority: 1
+    })
+  );
+})().catch(e => {
+  console.log(e);
+  process.exit(1);
+});
--- ops/create-index.ts
+++ ops/create-index.ts
@@ -1,19 +1,38 @@
 import * as faunadb from "faunadb";
-import * as dotenv from "dotenv";
-
-dotenv.config();
 
 const secret = process.env.FAUNADB_SERVER_SECRET || "";
-
 console.log("FAUNADB_SERVER_SECRET:", secret.replace(/\w/g, "*"));
 if (!secret) {
   console.log("FAUNADB_SERVER_SECRET is not set.");
   process.exit(1);
 }
+
 const q = faunadb.query as any;
 const client = new faunadb.Client({ secret });
 
 (async () => {
+  await client.query(
+    q.CreateCollection({
+      name: "endpoints",
+      history_days: 0,
+      ttl_days: 1
+    })
+  );
+  await client.query(
+    q.CreateCollection({
+      name: "results",
+      history_days: 0,
+      ttl_days: 1
+    })
+  );
+  await client.query(
+    q.CreateIndex({
+      name: "endpoints_by_key",
+      source: q.Collection("endpoints"),
+      terms: [{ field: ["data", "key"] }],
+      unique: true
+    })
+  );
   await client.query(
     q.CreateIndex({
       name: "results_by_key_order_by_requestedAt",
